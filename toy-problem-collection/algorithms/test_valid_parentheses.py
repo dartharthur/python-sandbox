d3b4f578dc513dfb386503d58ae0be7dafa7c322
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-01-17 19:18:25 -0800

    [refactor] DRYs-up test_valid_parentheses module

--- toy-problem-collection/algorithms/test_valid_parentheses.py
+++ toy-problem-collection/algorithms/test_valid_parentheses.py
@@ -2,17 +2,12 @@ import unittest
 from valid_parentheses import valid_parentheses
 
 class TestValidParentheses(unittest.TestCase):
-  def test_valid_parentheses(self):
-    result1 = valid_parentheses('()')
-    self.assertEqual(result1, True)
-    result2 = valid_parentheses(')(')
-    self.assertEqual(result2, False)
-    result3 = valid_parentheses('{()}')
-    self.assertEqual(result2, False)
-    result4 = valid_parentheses('(]')
-    self.assertEqual(result4, False)
-    result5 = valid_parentheses('([)]')
-    self.assertEqual(result5, False)
+    def test_valid_parentheses(self):
+        self.assertEqual(valid_parentheses('()'), True)
+        self.assertEqual(valid_parentheses(')('), False)
+        self.assertEqual(valid_parentheses('{()}'), False)
+        self.assertEqual(valid_parentheses('(]'), False)
+        self.assertEqual(valid_parentheses('([)]'), False)
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()

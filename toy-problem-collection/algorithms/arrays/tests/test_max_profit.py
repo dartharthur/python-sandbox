2275aada72824228be39d4c43bb96ff8bb764d37
Kevin Arthur <kevin.arthur.roy@gmail.com>
2018-03-21 18:04:57 -0700

    [docs] adds concise solution to max_profit as a comment

--- toy-problem-collection/algorithms/arrays/tests/test_max_profit.py
+++ toy-problem-collection/algorithms/arrays/tests/test_max_profit.py
@@ -2,7 +2,7 @@ import unittest
 from max_profit import max_profit
 
 
-class TestContainsDuplicate(unittest.TestCase):
+class TestMaxProfit(unittest.TestCase):
     def test_max_profit(self):
         self.assertEqual(max_profit([7, 1, 5, 3, 6, 4]), 5)
         self.assertEqual(max_profit([7, 6, 4, 3, 1]), 0)
